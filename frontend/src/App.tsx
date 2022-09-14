825870b4cc37ffffffc57095b2cc7eb9527b6139
Vinicius <pauloviniciusrp9@gmail.com>
2022-09-14 00:19:17 -0300

    INIT Auth0 login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,16 @@
+import LoginButton from './pages/login'
+import LogoutButton from './pages/logout'
+import Profile from './pages/profile'
+
 function App() {
-  return <h1>Hello Dolly</h1>
+  return (
+    <>
+      <h1>Hello Dolly</h1>
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </>
+  )
 }
 
 export default App
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
     <App />
+    </Auth0Provider>
   </React.StrictMode>
 )
