04c35eb1a7be84682635d68352dec962a3dee167
Vinicius <pauloviniciusrp9@gmail.com>
2022-10-08 19:29:21 -0300

    Commenting database procedure (set updated_at trigger)

--- ops/db/init.sql
+++ ops/db/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE dolly;
 \c dolly
 
 -- SETING TIMEZONE TO UTC
-SET timezone = 'UTC'
+SET timezone = 'UTC';
 
 -- CREATING STORE TABLE
 CREATE TABLE IF NOT EXISTS store(
@@ -20,21 +20,21 @@ CREATE TABLE IF NOT EXISTS mercadolivre_credentials(
   expires_in  INTEGER NOT NULL,
   user_id INTEGER NOT NULL,
   refresh_token VARCHAR(80) NOT NULL,
-  updated_at TIMESTAMPTZ,
+  updated_at TIMESTAMPTZ NOT NULL,
   PRIMARY KEY (owner_id)
 );
 
--- FUNCTION TO ADD updated_at (last update) IN Mercado Livre CREDENTIALS
-CREATE OR REPLACE FUNCTION trigger_set_updated_timestamp()
-RETURNS TRIGGER AS $$
-BEGIN
-  NEW.updated_at = NOW();
-  RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
+-- -- FUNCTION TO ADD updated_at (last update) IN Mercado Livre CREDENTIALS
+-- CREATE OR REPLACE FUNCTION trigger_set_updated_timestamp()
+-- RETURNS TRIGGER AS $$
+-- BEGIN
+--   NEW.updated_at = NOW();
+--   RETURN NEW;
+-- END;
+-- $$ LANGUAGE plpgsql;
 
--- CREATING TRIGGER TO ADD updated_at (last update) IN Mercado Livre CREDENTIALS
-CREATE TRIGGER set_updated_timestamp
-BEFORE UPDATE ON mercadolivre_credentials
-FOR EACH ROW
-EXECUTE PROCEDURE trigger_set_update_timestamp();
+-- -- CREATING TRIGGER TO ADD updated_at (last update) IN Mercado Livre CREDENTIALS
+-- CREATE TRIGGER set_updated_timestamp
+-- BEFORE UPDATE ON mercadolivre_credentials
+-- FOR EACH ROW
+-- EXECUTE PROCEDURE trigger_set_update_timestamp();
