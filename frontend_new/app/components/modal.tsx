b695875940e0822e52e8cc16eae37c2e06d894a2
Vinicius <pauloviniciusrp9@gmail.com>
2023-09-13 17:17:49 -0300

    Input Modal

--- frontend_new/app/components/modal.tsx
+++ frontend_new/app/components/modal.tsx
@@ -73,26 +73,25 @@ export default function Example() {
                     </div>
                   </div>
                   <div className="px-4 py-6 sm:p-8">
-                    <div className="sm:col-span-4">
-                      <label htmlFor="title" className="sr-only">
-                        Título
+                    <div className="flex gap-x-6">
+                      <label htmlFor="email-address" className="sr-only">
+                        Email address
                       </label>
-                      <div className="mt-2 flex gap-x4">
-                        <input
-                          id="title"
-                          name="title"
-                          maxLength={60}
-                          type="text"
-                          className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 px-3 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                          placeholder={"Título"}
-                        />
-                        <button
-                          type="submit"
-                          className="flex-none rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
-                        >
-                          <XMarkIcon className="h-5 w-4 text-white" />
-                        </button>
-                      </div>
+                      <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        maxLength={60}
+                        required
+                        className="min-w-0 flex-auto rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                        placeholder="Título do anúncio"
+                      />
+                      <button
+                        type="submit"
+                        className="flex-none rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
+                      >
+                        <XMarkIcon className="h-5 w-4 text-white" />
+                      </button>
                     </div>
                   </div>
                   <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
