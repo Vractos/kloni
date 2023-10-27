fc707fdabbdd4192b1ede317e7c5eb987ebf97c4
Vinicius <pauloviniciusrp9@gmail.com>
2023-10-27 00:10:15 -0300

    Fixing meta name

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -8,12 +8,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kloni",
-  icons: {
-    other: {
-      rel: "icon",
-      url: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🪨</text></svg>",
-    },
-  },
 };
 export default function RootLayout({
   children,
