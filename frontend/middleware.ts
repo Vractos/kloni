15f62f9141923ba0c02670afea140a76374755ca
Vinicius <pauloviniciusrp9@gmail.com>
2023-10-28 19:04:47 -0300

    Adding middleware matcher

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -11,3 +11,9 @@ export default withMiddlewareAuthRequired(
     }
     return res;
   });
+
+  export const config = {
+    matcher: [
+      '/((?!api|login|_next/static|_next/image|favicon.ico).*)',
+    ],
+  }
