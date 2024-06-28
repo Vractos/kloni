8153c67bfbb72f12c6d70880cb390a074db32ce3
Vinicius <pauloviniciusrp9@gmail.com>
2024-06-28 00:50:25 -0300

    chore: Add HashMap function to utils package

--- utils/slice.go
+++ utils/slice.go
@@ -1,5 +1,7 @@
 package utils
 
+import "reflect"
+
 func Contains[T Ordered](slice *[]T, v T) bool {
 	for _, e := range *slice {
 		if e == v {
@@ -22,4 +24,17 @@ func Chunk[T any](slice []T, chunkSize int) [][]T {
 		copy(chunks[i], slice[start:end])
 	}
 	return chunks
+}
+
+// HashMap is a helper function to convert a slice of structs into a map
+// where the key is the value of the field with the name keyField.
+//
+// The value of the map is the struct itself.
+func HashMap[T any](slice *[]T, keyField string) map[interface{}]T {
+	m := make(map[interface{}]T)
+	v := reflect.ValueOf((*slice)[0])
+	for _, e := range *slice {
+		m[v.FieldByName(keyField).Interface()] = e
+	}
+	return m
 }
