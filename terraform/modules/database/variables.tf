f9c8ed412827b0667b09ca45aa79afec164db22f
Vinicius <pauloviniciusrp9@gmail.com>
2024-06-29 17:36:14 -0300

    chore: Update AWS RDS engine version to 14.10

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -1,36 +1,36 @@
 variable "subnet_ids" {
-  type = set(string)
+  type     = set(string)
   nullable = false
 }
 
 variable "db_name" {
-  type = string
-  default = "kloni"
+  type    = string
+  default = "dolly"
 }
 
 variable "db_username" {
-  type = string
-  nullable = false
+  type      = string
+  nullable  = false
   sensitive = true
 }
 
 variable "db_password" {
-  type = string
-  nullable = false
+  type      = string
+  nullable  = false
   sensitive = true
 }
 
 variable "final_snapshot_identifier" {
-  type = string
-  default = "kloni-final-snapshot"
+  type    = string
+  default = "dolly-final-snapshot"
 }
 
 variable "db_security_group_ids" {
-  type = set(string)
+  type     = set(string)
   nullable = false
 }
 
 variable "project" {
-  type = string
-  default = "kloni"
+  type    = string
+  default = "dolly"
 }
