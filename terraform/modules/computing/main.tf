edbecbf340ec9a88fd52a973a8c1e2968d9f7761
Vinicius <pauloviniciusrp9@gmail.com>
2024-06-29 17:35:59 -0300

    Add ignore_changes for aws_iam_instance_profile

--- terraform/modules/computing/main.tf
+++ terraform/modules/computing/main.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "default_key" {
 }
 
 resource "aws_iam_role" "ec2_sqs_role" {
-  name = "ec2_access_sqs"
+  name        = "ec2_access_sqs"
   description = "Allows EC2 instances to call SQS"
 
   assume_role_policy = jsonencode({
@@ -34,17 +34,17 @@ resource "aws_iam_role_policy" "sqs_access_policy" {
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-        {
-            Effect = "Allow",
-            Action = [
-                "sqs:DeleteMessage",
-                "sqs:GetQueueUrl",
-                "sqs:ChangeMessageVisibility",
-                "sqs:ReceiveMessage",
-                "sqs:SendMessage"
-            ],
-            Resource = "arn:aws:sqs:${var.region}:${var.sdk_account_id}:*"
-        }
+      {
+        Effect = "Allow",
+        Action = [
+          "sqs:DeleteMessage",
+          "sqs:GetQueueUrl",
+          "sqs:ChangeMessageVisibility",
+          "sqs:ReceiveMessage",
+          "sqs:SendMessage"
+        ],
+        Resource = "arn:aws:sqs:${var.region}:${var.sdk_account_id}:*"
+      }
     ]
   })
 }
@@ -52,6 +52,12 @@ resource "aws_iam_role_policy" "sqs_access_policy" {
 resource "aws_iam_instance_profile" "dolly_instance_profile" {
   name = "${var.project}_server_profile"
   role = aws_iam_role.ec2_sqs_role.name
+
+  lifecycle {
+    ignore_changes = [
+      name
+    ]
+  }
 }
 
 resource "aws_instance" "dolly_server" {
@@ -66,7 +72,7 @@ resource "aws_instance" "dolly_server" {
 
 
   tags = {
-    Name        = "${var.instance_name} Server"
+    Name = "${var.instance_name} Server"
   }
 }
 
